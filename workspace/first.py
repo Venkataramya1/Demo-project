a1a411aefd8bb96a90a90fdb854822c7ff61c210
Kavala.Venkataramya <ramyakavala1@gmail.com>
2025-10-25 10:02:26 +0530

    first commit

---
+++ workspace/first.py
@@ -0,0 +1,6 @@
+#print("hello world")
+def mylist():
+    print("good morning")
+    print("hava a good day")
+mylist()
+print("welcome")
---
+++ workspace/variable.py
@@ -0,0 +1,8 @@
+num1=100
+num2=200
+sum=num1+num2
+diff=num1-num2
+mul=num1*num2
+div=num1/num2
+mod=num1%num2
+print(sum ,diff, mul, div, mod)
